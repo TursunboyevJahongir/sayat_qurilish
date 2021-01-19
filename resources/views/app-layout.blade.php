0cd284d091f75706dcaabbdb747e408a3cffe5a3
ensiente <javlonbek.sharipoff@gmail.com>
2021-01-19 15:54:11 +0500

    added layout

---
+++ resources/views/app-layout.blade.php
@@ -0,0 +1,163 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="theme-color" content="#feed01"/>
+    <title>Sayat Qurilish | Sanoat qurilish kompaniyasi</title>
+    <meta name="author" content="Javlonbek Sharipov">
+    <meta name="description" content="For all kind of construction company website">
+    <meta name="keywords" content="consto, business, construction, company, industrial, building, projects, corporate, apartments, flat, condo, brick, website, design, animation, transition, themezinho">
+
+    <!-- SOCIAL MEDIA META -->
+    <meta property="og:description" content="Consto | Industrial Construction Company">
+    <meta property="og:image" content="http://www.themezinho.net/consto/preview.png">
+    <meta property="og:site_name" content="consto">
+    <meta property="og:title" content="consto">
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="http://www.themezinho.net/consto">
+
+    <!-- TWITTER META -->
+{{--    <meta name="twitter:card" content="summary">--}}
+{{--    <meta name="twitter:site" content="@consto">--}}
+{{--    <meta name="twitter:creator" content="@consto">--}}
+{{--    <meta name="twitter:title" content="consto">--}}
+{{--    <meta name="twitter:description" content="Consto | Industrial Construction Company">--}}
+{{--    <meta name="twitter:image" content="http://www.themezinho.net/consto/preview.png">--}}
+
+<!-- FAVICON FILES -->
+{{--    <link href="ico/apple-touch-icon-144-precomposed.png" rel="apple-touch-icon" sizes="144x144">--}}
+{{--    <link href="ico/apple-touch-icon-114-precomposed.png" rel="apple-touch-icon" sizes="114x114">--}}
+{{--    <link href="ico/apple-touch-icon-72-precomposed.png" rel="apple-touch-icon" sizes="72x72">--}}
+{{--    <link href="ico/apple-touch-icon-57-precomposed.png" rel="apple-touch-icon">--}}
+{{--    <link href="ico/favicon.png" rel="shortcut icon">--}}
+
+<!-- CSS FILES -->
+    <link rel="stylesheet" href="{{asset('css/lineicons.css')}}">
+    <link rel="stylesheet" href="{{asset('css/odometer.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/fancybox.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/swiper.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/bootstrap.min.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
+</head>
+<body>
+<div class="first-transition"></div>
+<!-- end first-transition -->
+<div class="page-transition"></div>
+<!-- end page-transition -->
+<div class="search-box">
+    <div class="inner">
+        <form>
+            <input type="search" placeholder="Izlash uchun bosing">
+            <input type="submit" value="Izlash">
+        </form>
+    </div>
+</div>
+<!-- end search-box -->
+<aside class="side-widget">
+    <div class="inner">
+        <div class="logo"> <a href="index.html"><img src="images/logo.png" alt="Image"></a> </div>
+        <!-- end logo -->
+        <div class="hide-mobile">
+            <p>Extremity direction existence as dashwoods do up. Securing <u>marianne led</u> welcomed offended but offering six raptures. </p>
+            <figure class="gallery"><img src="images/slide02.jpg" alt="Image"><img src="images/slide03.jpg" alt="Image"></figure>
+            <h6 class="widget-title">ADDRESS</h6>
+            <address class="address">
+                <p>228 Cardigan Road, Leeds<br>
+                    Geneva Switzerland</p>
+                <p>+1 (850) 344 0 66 #20</p>
+            </address>
+            <h6 class="widget-title">FOLLOW US</h6>
+            <ul class="social-media">
+                <li><a href="#">Facebook</a></li>
+                <li><a href="#">Twitter</a></li>
+                <li><a href="#">Instagram</a></li>
+            </ul>
+        </div>
+        <!-- end hide-mobile -->
+        <div class="show-mobile">
+            <div class="languages">
+                <ul>
+                    <li><a href="#">en</a></li>
+                    <li><a href="#">ru</a></li>
+                </ul>
+            </div>
+            <!-- end languages -->
+            <div class="site-menu">
+                <ul>
+                    <li><a href="#">Consto</a><i class="lni lni-chevron-down-circle"></i>
+                        <ul>
+                            <li><a href="about-company.html">About Company</a></li>
+                            <li><a href="core-values.html">Core Values</a></li>
+                            <li><a href="leadership.html">Leadership</a></li>
+                            <li><a href="our-history.html">Our History</a></li>
+                            <li><a href="offices.html">Offices</a></li>
+                            <li><a href="certificates.html">Certificates</a></li>
+                        </ul>
+                    </li>
+                    <li><a href="services.html">Services</a></li>
+                    <li><a href="projects.html">Projects</a></li>
+                    <li><a href="news.html">News</a></li>
+                    <li><a href="contact.html">Contact</a></li>
+                </ul>
+            </div>
+            <!-- end site-menu -->
+        </div>
+        <!-- end show-mobile -->
+        <small>© 2020 Consto | Industrial Construction Company</small> </div>
+    <!-- end inner -->
+</aside>
+<!-- end side-widget -->
+<nav class="navbar">
+    <div class="container">
+        <div class="logo"> <a href="index.html" STYLE="color: #fff; font-size: 24px">SAYAT QURILISH</a> </div>
+        <!-- end logo -->
+    {{--        <div class="languages">--}}
+    {{--            <ul>--}}
+    {{--                <li><a href="#">en</a></li>--}}
+    {{--                <li><a href="#">ru</a></li>--}}
+    {{--            </ul>--}}
+    {{--        </div>--}}
+    <!-- end languages -->
+        <div class="site-menu">
+            <ul>
+                <li><a href="#">Sayat Qurilish</a>
+                    <ul>
+                        <li><a href="about-company.html">Kompaniya haqida</a></li>
+                        <li><a href="core-values.html">Core Values</a></li>
+                        <li><a href="leadership.html">Leadership</a></li>
+                        <li><a href="our-history.html">Our History</a></li>
+                        <li><a href="offices.html">Offices</a></li>
+                        <li><a href="certificates.html">Certificates</a></li>
+                    </ul>
+                </li>
+                <li><a href="services.html">Katalog</a></li>
+                <li><a href="services.html">Xizmatlar</a></li>
+                <li><a href="projects.html">Loyihalar</a></li>
+                <li><a href="news.html">Yangiliklar</a></li>
+            </ul>
+        </div>
+        <!-- end site-menu -->
+        <div class="search"> <i class="lni lni-search-alt"></i> </div>
+        <!-- end search -->
+
+        <div class="hamburger">
+            <div id="hamburger"> <span></span> <span></span> <span></span> </div>
+            <div id="cross"> <span></span> <span></span> </div>
+        </div>
+        <!-- end hamburger -->
+        <!-- end hamburher -->
+        <a href="#sales-specialist-form" class="navbar-button" data-fancybox><i class="lni lni-mobile"></i> <span>BIZ BILAN BOG'LANING</span></a> </div>
+    <!-- end container -->
+</nav>
+@yield('content')
+
+<script src="{{asset('js/jquery.min.js')}}"></script>
+<script src="{{asset('js/bootstrap.min.js')}}"></script>
+<script src="{{asset('js/swiper.min.js')}}"></script>
+<script src="{{asset('js/fancybox.min.js')}}"></script>
+<script src="{{asset('js/odometer.min.js')}}"></script>
+<script src="{{asset('js/isotope.min.js')}}"></script>
+<script src="{{asset('js/scripts.js')}}"></script>
+</body>
+</html>
