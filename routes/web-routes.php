93434e693939677b0eece920c907187483f74486
ensiente <javlonbek.sharipoff@gmail.com>
2021-01-20 23:23:10 +0500

    added web routes

--- routes/web-routes.php
+++ routes/web-routes.php
@@ -1,1 +1,3 @@
 <?php
+Route::get('projects', [\App\Http\Controllers\ProjectsController::class, 'index'])->name('project.index');
+Route::get('projects/{id}', [\App\Http\Controllers\ProjectsController::class, 'view'])->name('project.view');
