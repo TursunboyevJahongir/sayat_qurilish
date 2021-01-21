a06c87dac17dfccc2b0bee380282b29e6717859d
ensiente <javlonbek.sharipoff@gmail.com>
2021-01-21 12:03:31 +0500

    added props

--- app/Models/Employer.php
+++ app/Models/Employer.php
@@ -5,7 +5,21 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Class Employer
+ * @package App\Models
+ * @property int $id
+ * @property string $full_name
+ * @property string $image_url
+ * @property string $role
+ */
 class Employer extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'full_name',
+        'image_url',
+        'role'
+    ];
 }
